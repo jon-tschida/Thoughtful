187e6bbb27cf78b3a3efeb64f5328f0775c0e4a3
Jon Tschida <jon.tschida@yahoo.com>
2023-07-06 11:38:05 -0500

    style for lifequestion card
    added gradient bg, and conditional styling for the font size of the card content.

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 export default function QuestionCard({ topic, content }) {
   return (
-    <div className=" h-[500px] w-[350px] shadow-lg mx-3 my-1 shrink-0 bg-[#edf6f9] rounded-lg flex items-center justify-center cursor-pointer">
-      <p className="text-6xl font-light text-black h-fit">{topic || content}</p>
+    <div className=" h-[500px] w-[350px] shadow-lg mx-3 my-1 shrink-0 bg-[#edf6f9] rounded-lg flex items-center justify-center cursor-pointer p-4">
+      <p
+        className={`${
+          content ? `text-3xl` : `text-6xl`
+        } font-light text-black h-fit text-center`}
+      >
+        {topic || content}
+      </p>
     </div>
   );
 }
