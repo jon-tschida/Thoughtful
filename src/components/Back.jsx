cd057c2f679036408117329188f588fa796f0833
Jon Tschida <jon.tschida@yahoo.com>
2023-07-11 16:18:25 -0500

    removed stupid bg pic
    removed bg pic and made the app component a linear gradient bg

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -7,7 +7,7 @@ export default function Back() {
     <Link to={"/"}>
       <img
         src={BackArrow}
-        className="absolute transition-all duration-300 cursor-pointer opacity-90 w-7 top-10 left-10 hover:scale-110"
+        className="absolute transition-all duration-300 cursor-pointer select-none opacity-90 w-7 top-10 left-10 hover:scale-110"
       />
     </Link>
   );
