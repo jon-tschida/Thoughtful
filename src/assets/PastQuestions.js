7e6ffe154e4d3c41bd1fd842310c19da3d9b013c
Jon Tschida <jon.tschida@yahoo.com>
2023-07-15 17:00:42 -0500

    added questions
    added past and relationship questions

--- src/assets/PastQuestions.js
+++ src/assets/PastQuestions.js
@@ -1,6 +1,10 @@
 export const pastQs = [
   "What's your favorite childhood memory?",
   "Do you have any regrets from your past?",
+  "What's your most embarrasing teen moment?",
+  "Who was your first crush?",
+  "Who was an important person to you growing up?",
+  "What was the worst argument you had with a friend?",
 ];
 
 export default {
--- src/assets/RelationshipQuestions.js
+++ src/assets/RelationshipQuestions.js
@@ -1,6 +1,10 @@
 export const RelationshipQs = [
   "If given the chance to date a celebrity who would it be?",
   "What is your favorite thing about me?",
+  "What was our worst dat?",
+  "What was our best date?",
+  "How do you express your feelings?",
+  "If anything, what'd your parents teach you about relationships?",
 ];
 
 export default { RelationshipQs };
