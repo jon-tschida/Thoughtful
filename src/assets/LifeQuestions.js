f7afa383cce6ff03edfba48af7c040512d1c47f9
Jon Tschida <jon.tschida@yahoo.com>
2023-07-15 17:02:40 -0500

    Updated life questions

--- src/assets/LifeQuestions.js
+++ src/assets/LifeQuestions.js
@@ -2,5 +2,6 @@ export const lifeQs = [
   "What movie or show do you keep rewatching?",
   "What was your favorite game as a child?",
   "Which book or movie impacted you most growing up?",
+  "What is the biggest lie you've told?",
 ];
 export default { lifeQs };
