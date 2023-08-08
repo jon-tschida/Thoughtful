1b8d9f4ee26edd98b69421aa443ec57e0a0e083e
Jon Tschida <jon.tschida@yahoo.com>
2023-08-08 15:46:04 -0500

    Updated ErrorPage.jsx
    We updated our error page to display either a 404 page, or, a page to submit a bug report if the actual JS breaks.

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,28 @@
 import { useRouteError } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 export default function ErrorPage() {
   const error = useRouteError();
+  console.log(error.status === 404);
 
-  return (
+  return error.status === 404 ? (
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-zinc-200">
+      <h1 className="mb-5 text-5xl font-extrabold">Oops!</h1>
+      <p className="mb-5">It looks like this page doesn't exist...</p>
+      <p className="font-light text-gray-600">
+        <i>
+          {error.status} {error.statusText || error.message}
+        </i>
+      </p>
+      <Link to={"/"}>
+        <br />
+        <p className="">
+          <i className="p-2 transition-all border border-solid rounded-md border-zinc-700 bg-zinc-50 hover:bg-zinc-200">
+            Back to topics
+          </i>
+        </p>
+      </Link>
+    </div>
+  ) : (
     <div className="flex flex-col items-center justify-center w-screen h-screen bg-zinc-200">
       <h1 className="mb-5 text-5xl font-extrabold">Oops!</h1>
       <p className="mb-5">Sorry, an unexpected error has occurred.</p>
@@ -11,6 +30,16 @@ export default function ErrorPage() {
         <i>
           {error.status} {error.statusText || error.message}
         </i>
+      </p>
+      <br />
+      <p className="p-2 transition-all border border-solid rounded-md border-zinc-700 bg-zinc-50 hover:bg-zinc-200">
+        <a
+          href="https://forms.gle/96Q97u1EPog3BiLU7"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i>Submit Bug Report</i>
+        </a>
       </p>
     </div>
   );
